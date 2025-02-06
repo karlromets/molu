10cab11e988469be52f14d8a78ad69a1887cfb4d
Karl Romet Somelar <108818570+karlromets@users.noreply.github.com>
2025-02-06 19:59:20 +0200

    feat(tailwind): update config with ES module syntax and add button shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-const {withAnimations} = require("animated-tailwindcss");
+import { withAnimations } from "animated-tailwindcss";
 
 /** @type {import('tailwindcss').Config} */
-module.exports = withAnimations({
+export default withAnimations({
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {
@@ -9,6 +9,9 @@ module.exports = withAnimations({
         blueToBeige:
           "linear-gradient(180deg, rgba(11, 191, 251, 1) 1%, rgba(11, 191, 251, 1) 35%, rgba(240, 234, 214, 1) 50%, rgba(240, 234, 214, 1) 100%)",
       },
+      boxShadow: {
+        "button": '-10px 10px #000'
+      }
     },
   },
   plugins: [],
